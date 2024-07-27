2eca02c67d857997ebfdef7ef0a890bd211217cd
Marshal-Rommel <subhadipbiswas12d@gmail.com>
2024-07-27 14:51:55 +0530

    Mod 1
    Added styles.css

--- testapp/src/index.jsx
+++ testapp/src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <h1 style= {{textAlign: "center"}}>
-      Hello World
-    </h1>
+    <h1 style= {{textAlign: "center"}}> Hello World </h1>
+    <p className="para"> This is a sample paragraph </p>
   </>
 );
