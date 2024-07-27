8c7b5af9dcb2eaa2944c208d580b705b0d9a18be
Marshal-Rommel <subhadipbiswas12d@gmail.com>
2024-07-27 16:16:59 +0530

    Mod 2b
    Reprogrammed Clock.jsx with auto-updating clock

--- testapp/src/Clock.jsx
+++ testapp/src/Clock.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Clock(){
-    const time = new Date().toLocaleTimeString();
+function Clock() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-    return (
-        <>
-            <p style={{textAlign:"center"}}>Time: {time}</p>
-        </>
+    useEffect(
+        function setupInterval() {
+            const intervalId = setInterval(function updateTime() {setTime(new Date().toLocaleTimeString());}, 1000);   
+            return function cleanup() {clearInterval(intervalId)};
+      }
     );
-    
-}
 
+    return <p style={{ textAlign: 'center' }}>Time: {time}</p>;
+}
 export default Clock;
