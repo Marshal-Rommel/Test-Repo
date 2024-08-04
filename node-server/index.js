b07b2bdea6da1b4aa413abae73ceb8687a89a132
Marshal-Rommel <subhadipbiswas12d@gmail.com>
2024-08-04 15:19:25 +0530

    Updated index.js
    Added an about page

--- node-server/index.js
+++ node-server/index.js
@@ -6,6 +6,10 @@ app.get('/',function(req,res){
     res.send('Hello World!');
 });
 
+app.get('/about',function(req,res){
+    res.send('Simple server using node.js');
+});
+
 app.listen(port,function(){
     console.log("server started... go to localhost:3000");
 });
