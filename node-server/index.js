28929e217a582c9da2f1eb1a7c6482eb247361ed
Marshal-Rommel <subhadipbiswas12d@gmail.com>
2024-08-04 16:14:49 +0530

    Modified and updated files
    Practicing operation using html files

--- node-server/index.js
+++ node-server/index.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get('/',function(req,res){
-    res.send('Hello World!');
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/about',function(req,res){
-    res.send('Simple server using node.js');
+    res.sendFile(__dirname + "/about.html");
 });
+
+app.get('*',function(req,res){
+    res.sendFile(__dirname + "/error.html");
+  });
 
 app.listen(port,function(){
     console.log("server started... go to localhost:3000");
